919e86bd8aebf69d297afe715126e74391501aa1
blankverse123000 <blankverse123000@gmail.com>
2022-12-20 22:06:38 +0630

    DarkMode_edited

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,18 @@
 /*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+let navChange = document.querySelector(".navChange");
+let body = document.querySelector("body");
+let themeBtn = document.querySelector(".themeBtn");
+let theme_icon = document.querySelector(".theme_icon");
+function darkTheme() {
+  body.classList.toggle("darkMode");
+  navChange.classList.toggle("navbar-dark");
+  navChange.classList.toggle("bg-dark");
+  navChange.classList.toggle("bg-body");
+  themeBtn.classList.toggle("text-white");
+  theme_icon.classList.toggle("ri-moon-line");
+  theme_icon.classList.toggle("ri-sun-line");
+}
+
+themeBtn.addEventListener("click", darkTheme);
